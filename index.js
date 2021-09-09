df2f6b6ef68a7726d38e1bf068f2adcef117d0c1
ACarrill42 <austincarrill@gmail.com>
2021-09-09 09:07:24 -0500

    working on router

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const server = require("./api/server.js");
+
+const port  = process.env.PORT || 5678;
+server.listen(port, () => {
+  console.log(`Server running on localhost:${port}`);
+})
